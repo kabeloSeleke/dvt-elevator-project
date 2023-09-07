f26c5ec53e22cf020fabad16c7c77464220a25f3
kabelo seleke <kabza.seleke@gmail.com>
2023-09-07 16:56:22 +0200

    feat(tests): Implement unit tests for ElevatorRepository and FloorRepository
    Summary:
    Added unit tests for both `ElevatorRepository` and `FloorRepository` to validate their CRUD operations and ensure the correct behavior of the data access layer.
    
    Details:
    1. **Testing Framework**:
       - Utilized xUnit as the testing framework to structure and run the tests.
    
    2. **In-Memory Database**:
       - Configured an in-memory database for testing purposes, ensuring isolated test environments without affecting the actual database.
    
    3. **ElevatorRepository Test Cases**:
       - `AddElevator_AddsElevatorSuccessfullyAsync`: Validates the addition of a new elevator.
       - `GetElevatorById_ReturnsCorrectElevatorAsync`: Ensures retrieval of the correct elevator by its ID.
       - `RemoveElevator_RemovesElevatorSuccessfullyAsync`: Tests the removal of an elevator from the database.
    
    4. **FloorRepository Test Cases**:
       - `AddFloor_AddsFloorSuccessfullyAsync`: Validates the addition of a new floor.
       - `GetFloorById_ReturnsCorrectFloorAsync`: Ensures retrieval of the correct floor by its ID.
       - `RemoveFloor_RemovesFloorSuccessfullyAsync`: Tests the removal of a floor from the database.
       - `UpdateFloor_UpdatesFloorSuccessfullyAsync`: Validates the update operation for a floor's attributes.
       - `GetAllFloors_ReturnsAllFloorsAsync`: Ensures all floors are retrieved correctly.

---
+++ ElevatorSystem.Tests/Repositories/ElevatorRepositoryTests.cs
@@ -0,0 +1,131 @@
+﻿using ElevatorSystem.Domain.Models;
+using ElevatorSystem.Domain.Values;
+using ElevatorSystem.Infrastructure.Data;
+using ElevatorSystem.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElevatorSystem.Tests.Repositories {
+    public class ElevatorRepositoryTests {
+        private readonly ApplicationDbContext _context;
+        private readonly ElevatorRepository _elevatorRepository;
+
+        public ElevatorRepositoryTests() {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new ApplicationDbContext(options);
+            _elevatorRepository = new ElevatorRepository(_context);
+        }
+
+        [Fact]
+        public async Task AddElevator_AddsElevatorSuccessfullyAsync() {
+            var elevator = new Elevator {
+                Id = 1,
+                CurrentFloor = 1,
+                Status = ElevatorStatus.Idle,
+                Direction = ElevatorDirection.None
+            };
+
+            await _elevatorRepository.AddElevatorAsync(elevator);
+
+            var result = await _elevatorRepository.GetElevatorByIdAsync(1);
+            Assert.NotNull(result);
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async Task GetElevatorById_ReturnsCorrectElevatorAsync() {
+            var elevator = new Elevator { Id = 2, CurrentFloor = 2, Status = ElevatorStatus.Idle, Direction = ElevatorDirection.None };
+            _context.Elevators.Add(elevator);
+            await _context.SaveChangesAsync();
+            var result = await _elevatorRepository.GetElevatorByIdAsync(2);
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Id);
+        }
+
+        [Fact]
+        public async Task RemoveElevator_RemovesElevatorSuccessfullyAsync() {
+            var elevator = new Elevator { Id = 3, CurrentFloor = 3, Status = ElevatorStatus.Idle, Direction = ElevatorDirection.None };
+            _context.Elevators.Add(elevator);
+            await _context.SaveChangesAsync();
+
+            await _elevatorRepository.RemoveElevatorAsync(3);
+            var result = await _elevatorRepository.GetElevatorByIdAsync(3);
+
+            Assert.Null(result);
+        }
+    }
+}
+
+namespace ElevatorEngine.Tests.Repositories {
+    public class FloorRepositoryTests {
+        private readonly ApplicationDbContext _context;
+        private readonly FloorRepository _floorRepository;
+
+        public FloorRepositoryTests() {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new ApplicationDbContext(options);
+            _floorRepository = new FloorRepository(_context);
+        }
+
+        [Fact]
+        public async Task AddFloor_AddsFloorSuccessfullyAsync() {
+            var floor = new Floor { Id = 1, FloorNumber = 1, WaitingOccupants = 5, TotalPeopleGoingUp = 3, TotalPeopleGoingDown = 2 };
+            await _floorRepository.AddFloorAsync(floor);
+
+            var result = await _floorRepository.GetFloorByIdAsync(1);
+            Assert.NotNull(result);
+            Assert.Equal(1, result.Id);
+        }
+
+        [Fact]
+        public async Task GetFloorById_ReturnsCorrectFloorAsync() {
+            var floor = new Floor { Id = 2, FloorNumber = 2, WaitingOccupants = 3, TotalPeopleGoingUp = 2, TotalPeopleGoingDown = 1 };
+            _context.Floors.Add(floor);
+            await _context.SaveChangesAsync();
+            var result = await _floorRepository.GetFloorByIdAsync(2);
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Id);
+        }
+
+        [Fact]
+        public async Task RemoveFloor_RemovesFloorSuccessfullyAsync() {
+            var floor = new Floor { Id = 3, FloorNumber = 3, WaitingOccupants = 4, TotalPeopleGoingUp = 2, TotalPeopleGoingDown = 2 };
+            _context.Floors.Add(floor);
+            await _context.SaveChangesAsync();
+
+            await _floorRepository.RemoveFloorAsync(3);
+            var result = await _floorRepository.GetFloorByIdAsync(3);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task UpdateFloor_UpdatesFloorSuccessfullyAsync() {
+            var floor = new Floor { Id = 4, FloorNumber = 4, WaitingOccupants = 5, TotalPeopleGoingUp = 3, TotalPeopleGoingDown = 2 };
+            _context.Floors.Add(floor);
+            await _context.SaveChangesAsync();
+            floor.WaitingOccupants = 10;
+            await _floorRepository.UpdateFloorAsync(floor);
+            var updatedFloor = await _floorRepository.GetFloorByIdAsync(4);
+            Assert.Equal(10, updatedFloor.WaitingOccupants);
+        }
+
+        [Fact]
+        public async Task GetAllFloors_ReturnsAllFloorsAsync() {
+            var floor1 = new Floor { Id = 5, FloorNumber = 5, WaitingOccupants = 6, TotalPeopleGoingUp = 4, TotalPeopleGoingDown = 2 };
+            var floor2 = new Floor { Id = 6, FloorNumber = 6, WaitingOccupants = 7, TotalPeopleGoingUp = 5, TotalPeopleGoingDown = 2 };
+
+            _context.Floors.AddRange(floor1, floor2);
+            await _context.SaveChangesAsync();
+
+            var result = await _floorRepository.GetAllFloorsAsync();
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count());
+        }
+    }
+}
--- ElevatorSystem.Tests/UnitTest1.cs
+++
@@ -1,8 +0,0 @@
-namespace ElevatorSystem.Tests {
-    public class UnitTest1 {
-        [Fact]
-        public void Test1() {
-
-        }
-    }
-}
