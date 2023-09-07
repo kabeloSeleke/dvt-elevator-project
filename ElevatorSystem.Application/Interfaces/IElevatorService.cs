a83768ad48c0e5791198def2c0806a3dfa356244
kabelo seleke <kabza.seleke@gmail.com>
2023-09-07 19:17:17 +0200

    feat(application-layer): Introduce Service Interfaces for Elevator and Floor Summary: Introduced service interfaces IElevatorService and IFloorService to define contracts for elevator and floor operations in the application layer.
    Details:
    IElevatorService:
    
    GetElevatorStatusAsync: Retrieve the status of a specific elevator.
    UpdateElevatorAsync: Update the attributes of an elevator.
    CreateElevatorAsync: Add a new elevator to the system.
    GetAllElevatorsAsync: Fetch all elevators in the system.
    GetAllElevatorStatusesAsync: Retrieve the statuses of all elevators.
    GetNearestAvailableElevatorAsync: Find the closest available elevator for a specific floor and direction.
    SendNearestElevatorToFloorAsync: Dispatch the nearest elevator to a specified floor.
    IFloorService:
    
    GetFloorStatusAsync: Retrieve the status of a specific floor.
    UpdateFloorAsync: Update the attributes of a floor.
    CreateFloorAsync: Add a new floor to the system.
    RequestElevatorToFloorAsync: Request an elevator to a specific floor with the number of people and desired direction.
    GetAllFloorStatusesAsync: Fetch the statuses of all floors.
    UpdateFloorOccupantsAsync: Update the number of occupants on a specific floor with their desired direction.
    Next Steps:
    Implement concrete service classes based on these interfaces.
    Integrate these services with the existing repositories and DTOs.
    Ensure proper error handling and validation in the service implementations.

---
+++ ElevatorSystem.Application/Interfaces/IElevatorService.cs
@@ -0,0 +1,19 @@
+﻿using ElevatorSystem.Application.DTOs;
+using ElevatorSystem.Domain.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorSystem.Application.Interfaces {
+    public interface IElevatorService {
+        Task<ElevatorDTO> GetElevatorStatusAsync(int elevatorId);
+        Task UpdateElevatorAsync(ElevatorDTO elevatorDTO);
+        Task<ElevatorDTO> CreateElevatorAsync(ElevatorDTO elevatorDTO);
+        Task<IEnumerable<ElevatorDTO>> GetAllElevatorsAsync();
+        Task<IEnumerable<ElevatorDTO>> GetAllElevatorStatusesAsync();
+        Task<ElevatorDTO> GetNearestAvailableElevatorAsync(int floorId, ElevatorDirection direction);
+        Task SendNearestElevatorToFloorAsync(int floorNumber, ElevatorDirection direction);
+    }
+}
